c5bb36c9cff58d3f9791be2615d0137923e15fc5
Lucas <lucasbrodersen@gmail.com>
2023-08-28 18:40:36 +0100

    customer and service filter working

---
+++ restAPI/restAPI/app/Http/Requests/V1StoreCustomerRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class V1StoreCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'type' => ['required', Rule::in(['I', 'i', 'B', 'b'])],
+            'email' => ['required', 'email'],
+            'address' => ['required'],
+            'city' => ['required'],
+            'state' => ['required'],
+            'postalCode' => ['required'],
+        ];
+    }
+}
--- restAPI/restAPI/app/Models/Customer.php
+++ restAPI/restAPI/app/Models/Customer.php
@@ -10,6 +10,16 @@ class Customer extends Model
     use HasFactory;
     protected $connection = 'mongodb';
 
+    protected $fillable = [
+        'name',
+        'type',
+        'email',
+        'address',
+        'city',
+        'state',
+        'postalCode'
+    ];
+
     public function services(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Service::class);
